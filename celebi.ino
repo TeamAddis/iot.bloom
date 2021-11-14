1767432d7bdd9aeee2470de25ae06c07fbc2d091
Andrew Addis <addisandrew1@gmail.com>
2021-11-14 19:47:43 +0900

    Updated to connect to wifi and update RTC
    Now correctly connects to wifi and updates the RTC
    Setting alarms to toggle the pump now works.

--- celebi.ino
+++ celebi.ino
@@ -11,21 +11,29 @@
  * Includes
  */
 #include <RTCZero.h>
+#include <WiFiNINA.h>
+#include "network_secrets.h"
 
 /* 
  * Pump variables
  */
-bool activatePump = false;
+bool pumpIsActive = false;
+// #define PUMP_PIN D6
 
 /* 
  * RTC (Real time clock) variables
  */
-const byte seconds = 0;
-const byte minutes = 2;
-const byte hours = 19;
-
 RTCZero rtc;
 
+/* 
+ * Wifi variables
+ */
+char ssid[] = SECRET_SSID;
+char pass[] = SECRET_PASS;
+
+int status = WL_IDLE_STATUS;
+
+const int GMT = 9;
 
 /* 
  * Setup function
@@ -34,18 +42,42 @@ RTCZero rtc;
  * 
  */
 void setup() {
-    Serial.begin(9600);
+    Serial.begin(115200);
+
+    // Setup the wifi module.
+    if (!setupWifi()) {while(true);}
 
     // Configure the RTC
     rtc.begin();
-    rtc.setTime(hours, minutes, seconds);
+    
+    unsigned long epoch = 0;
+    int numberOfTries = 0, maxTries = 6;
 
-    // Set the alarm
-    setAlarm(19, 5, 0);
-    rtc.enableAlarm(rtc.MATCH_HHMMSS);
-    rtc.attachInterrupt(togglePump);
+    do {
+        epoch = WiFi.getTime();
+        numberOfTries++;
+    } while ((epoch == 0) && (numberOfTries < maxTries));
 
-    pinMode(LED_BUILTIN, OUTPUT);   // initialize digital pin LED_BUILTIN as an output.
+    if (numberOfTries == maxTries) {
+        Serial.println("NTP unreachable!!");
+        while(1);
+    } else {
+        Serial.print("Time received: ");
+        Serial.println(epoch);
+
+        rtc.setEpoch(epoch);
+        Serial.println();
+    }
+
+    pinMode(LED_BUILTIN, OUTPUT);
+
+    // Set the test alarm
+    testAlarm(2);
+}
+
+void testAlarm(byte secondsFromNow) {
+    byte newSeconds = rtc.getSeconds() + secondsFromNow;
+    setAlarm(rtc.getHours(), rtc.getMinutes(), newSeconds);
 }
 
 /* 
@@ -55,26 +87,64 @@ void setup() {
  * 
  */
 void loop() {
-    if (activatePump) {
-        digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
-        Serial.println("Turned pump on");
-    } else {
-        digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
-        Serial.println("Turned pump off");
-    }
+    
 }
 
 /* 
  * Toggle the submersable pump
  */
 void togglePump() {
-    activatePump = !activatePump;
+    if (pumpIsActive) {
+        // Turn off the pump
+        Serial.println("Turning Pump off");
+        digitalWrite(LED_BUILTIN, LOW);
+        pumpIsActive = !pumpIsActive;
+    } else {
+        // Turn on the pump
+        Serial.println("Turning Pump on");
+        digitalWrite(LED_BUILTIN, HIGH);
+        pumpIsActive = !pumpIsActive;
+
+        // Set alarm for turning off the pump
+        testAlarm(10);
+    }
 }
 
 /* 
  * Set alarm
  */
 void setAlarm(byte hours, byte minutes, byte seconds) {
+    Serial.println("alarm set");
     rtc.setAlarmTime(hours, minutes, seconds);
-    rtc.setAlarmTime(hours, minutes, seconds + 5);
+    rtc.enableAlarm(rtc.MATCH_HHMMSS);
+    rtc.attachInterrupt(togglePump);
+}
+
+/* 
+ * Setup the wifi chip
+ */
+bool setupWifi() {
+    if (WiFi.status() == WL_NO_MODULE) {
+        Serial.println("Communication with WiFi module failed!");
+        return false;
+    }
+
+    // Check the firmware version
+    String fv = WiFi.firmwareVersion();
+    if (fv < WIFI_FIRMWARE_LATEST_VERSION) {
+        Serial.println("Please upgrade the firmware");
+    }
+
+    // attempt to connect to WiFi network:
+    while (status != WL_CONNECTED) {
+        Serial.print("Attempting to connect to Network named: ");
+        Serial.println(ssid);                   // print the network name (SSID);
+
+        status = WiFi.begin(ssid, pass);
+        
+        // wait 10 seconds for connection:
+        delay(10000);
+    }
+
+    return true;
 }
